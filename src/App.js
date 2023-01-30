10b7a48488e418592b1a932294aa2b5e94749312
Michael Humphries <michaelhumphries1997@gmail.com>
2023-01-30 22:08:40 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import queryString from "query-string";
+import axios from "axios";
+import { useCallback } from "react";
+
+const parseQueryString = (search) => {
+  const parsed = queryString.parse(search);
+  const access_token = (parsed && parsed.access_token) || undefined;
+  const refresh_token = (parsed && parsed.refresh_token) || undefined;
+  return { access_token, refresh_token };
+};
+// parseQueryString(window.location.search)
+
+const fetchData = async (query) => {
+  const { access_token, refresh_token } = parseQueryString(query);
+  const response = await axios.get("https://api.spotify.com/v1/me", {
+    headers: { Authorization: `Bearer ${access_token}` },
+  });
+
+  return response && response.data ? response.data : undefined;
+};
 
 function App() {
+  console.log("FOOO", window.location.hash.substring(1));
+  const foo = useCallback(async () => await fetchData(window.location.hash));
+  console.log(foo());
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {}
         </p>
         <a
           className="App-link"
@@ -17,6 +41,7 @@ function App() {
         >
           Learn React
         </a>
+        <a href="http://localhost:8080/auth/login">Log in with Spotify</a>
       </header>
     </div>
   );
