d4eb681a0e68298c1b74e2d2d91ea908b6d81fd2
Michael Humphries <michaelhumphries1997@gmail.com>
2023-01-31 11:59:23 +0000

    adding stuff to call backend

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 import queryString from "query-string";
 import axios from "axios";
 import { useCallback } from "react";
+import {Button} from "@material-ui/core"
 
-const parseQueryString = (search) => {
-  const parsed = queryString.parse(search);
-  const access_token = (parsed && parsed.access_token) || undefined;
-  const refresh_token = (parsed && parsed.refresh_token) || undefined;
-  return { access_token, refresh_token };
+const parseQueryString = (href) => {
+  const parsed = queryString.parse(href);
+  const code = (parsed && parsed.code) || undefined;
+  const state = (parsed && parsed.state) || undefined;
+  return { code, state };
 };
 // parseQueryString(window.location.search)
 
-const fetchData = async (query) => {
-  const { access_token, refresh_token } = parseQueryString(query);
-  const response = await axios.get("https://api.spotify.com/v1/me", {
-    headers: { Authorization: `Bearer ${access_token}` },
-  });
-
-  return response && response.data ? response.data : undefined;
+const fetchData = async (href) => {
+  // const { code, state } = parseQueryString(href);
+  // const response = await axios.get("https://api.spotify.com/v1/me", {
+  //   headers: { Authorization: `Bearer ${access_token}` },
+  // });
+  const response = await axios.get(`http://localhost:8080/auth/callback/${href}`);
+  console.log(response)
+  // return response && response.data ? response.data : undefined;
 };
 
 function App() {
-  console.log("FOOO", window.location.hash.substring(1));
-  const foo = useCallback(async () => await fetchData(window.location.hash));
+  console.log("FOOO", window.location.search);
+  const foo = useCallback(async () => await fetchData(window.location.search));
   console.log(foo());
   return (
     <div className="App">
@@ -41,7 +43,7 @@ function App() {
         >
           Learn React
         </a>
-        <a href="http://localhost:8080/auth/login">Log in with Spotify</a>
+        <Button href="http://localhost:8080/auth/login">Log in with Spotify</Button>
       </header>
     </div>
   );
